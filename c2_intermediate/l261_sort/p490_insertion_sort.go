975bc67315e9466725461556699476f4d521d2bf
hartun9 <h4rtun9@gmail.com>
2025-09-26 21:53:00 +0900

    feat: add insertionSort

---
+++ c2_intermediate/l261_sort/p490_insertion_sort.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func insertionSort(arr []int32) []int32 {
+	sortedIndex := 0
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j <= sortedIndex; j++ {
+			if arr[i] < arr[j] {
+				tmp := arr[i]
+				for k := i - 1; k >= j; k-- {
+					arr[k+1] = arr[k]
+				}
+				arr[j] = tmp
+				break
+			}
+		}
+		sortedIndex++
+	}
+	return arr
+}
+
+func main() {
+	fmt.Println(insertionSort([]int32{2, 12, 5, 10, 9, 8}))
+}
