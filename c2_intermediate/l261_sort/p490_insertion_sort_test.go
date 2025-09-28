45328a64a85cb60033a72ac030390571e49bdfbb
hartun9 <h4rtun9@gmail.com>
2025-09-28 14:56:36 +0900

    test: add TestInsertionSort

---
+++ c2_intermediate/l261_sort/p490_insertion_sort_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestInsertionSort(t *testing.T) {
+	tests := []struct {
+		name string
+		arr  []int32
+		want []int32
+	}{
+		{"normal", []int32{2, 4, 1, 5, 9}, []int32{1, 2, 4, 5, 9}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := insertionSort(tt.arr)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("selectionSort(%v) = %d; want %d", tt.arr, got, tt.want)
+			}
+		})
+	}
+}
