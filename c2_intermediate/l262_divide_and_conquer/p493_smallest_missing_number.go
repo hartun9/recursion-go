9a633d5e72e3a3d8c85ae8ee54ea192fcafba0e5
hartun9 <h4rtun9@gmail.com>
2025-10-01 10:14:02 +0900

    feat: indexと比較する方針に変更

--- c2_intermediate/l262_divide_and_conquer/p493_smallest_missing_number.go
+++ c2_intermediate/l262_divide_and_conquer/p493_smallest_missing_number.go
@@ -1,40 +1,20 @@
 package main
 
 func smallestMissingNumber(arr []int32) int32 {
-	min, _ := smallestMissingNumberHelper(arr, 0, len(arr)-1)
-	return min
+	return smallestMissingNumberHelper(arr, 0, len(arr)-1)
 }
 
-func smallestMissingNumberHelper(arr []int32, start int, end int) (min int32, isPresent bool) {
+func smallestMissingNumberHelper(arr []int32, start int, end int) int32 {
 	if start == end {
-		min = arr[start]
-		isPresent = false
-		return min, isPresent
-	} else if start+1 == end {
-		if arr[start]+1 == arr[end] {
-			min = arr[end]
-			isPresent = false
-			return min, isPresent
-		} else {
-			min = arr[start] + 1
-			isPresent = true
-			return min, isPresent
+		if start != int(arr[start]) {
+			return arr[start] - 1
 		}
+		return arr[start] + 1
 	}
-	middle := (start + end) / 2
-	minLeft, isPresentLeft := smallestMissingNumberHelper(arr, start, middle)
-
-	if isPresentLeft {
-		return minLeft, true
+	mid := (start + end) / 2
+	if mid < int(arr[mid]) {
+		return smallestMissingNumberHelper(arr, start, mid)
 	} else {
-		if arr[middle]+1 != arr[middle+1] {
-			return arr[middle] + 1, true
-		}
+		return smallestMissingNumberHelper(arr, mid+1, end)
 	}
-
-	minRight, isPresentRight := smallestMissingNumberHelper(arr, middle+1, end)
-	if isPresentRight {
-		return minRight, true
-	}
-	return minRight + 1, false
 }
